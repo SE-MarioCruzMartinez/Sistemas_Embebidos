d3a3040c5b477fd39bec537d568f863873555c48
SE-MarioCruzMartinez <72464046+SE-MarioCruzMartinez@users.noreply.github.com>
2020-11-24 10:58:02 -0600

    Actualiacion 07 Menu PC

---
+++ Actividades/07_Menu basico de PC/Codigo/ARDUINO_RGB/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace ARDUINO_RGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonConectar = new System.Windows.Forms.Button();
+            this.comboBox_velocidad = new System.Windows.Forms.ComboBox();
+            this.comboBox_Puerto = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_B = new System.Windows.Forms.TextBox();
+            this.textBox_G = new System.Windows.Forms.TextBox();
+            this.textBox_R = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar_B = new System.Windows.Forms.TrackBar();
+            this.trackBar_G = new System.Windows.Forms.TrackBar();
+            this.trackBar_R = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PuertoSerie = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_G)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_R)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonConectar);
+            this.groupBox1.Controls.Add(this.comboBox_velocidad);
+            this.groupBox1.Controls.Add(this.comboBox_Puerto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 183);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CONFIGURACION";
+            // 
+            // buttonConectar
+            // 
+            this.buttonConectar.BackColor = System.Drawing.Color.Green;
+            this.buttonConectar.Location = new System.Drawing.Point(144, 140);
+            this.buttonConectar.Name = "buttonConectar";
+            this.buttonConectar.Size = new System.Drawing.Size(121, 26);
+            this.buttonConectar.TabIndex = 4;
+            this.buttonConectar.Text = "CONECTAR";
+            this.buttonConectar.UseVisualStyleBackColor = false;
+            this.buttonConectar.Click += new System.EventHandler(this.buttonConectar_Click);
+            // 
+            // comboBox_velocidad
+            // 
+            this.comboBox_velocidad.FormattingEnabled = true;
+            this.comboBox_velocidad.Location = new System.Drawing.Point(144, 92);
+            this.comboBox_velocidad.Name = "comboBox_velocidad";
+            this.comboBox_velocidad.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_velocidad.TabIndex = 3;
+            this.comboBox_velocidad.SelectedIndexChanged += new System.EventHandler(this.comboBox_velocidad_SelectedIndexChanged);
+            // 
+            // comboBox_Puerto
+            // 
+            this.comboBox_Puerto.FormattingEnabled = true;
+            this.comboBox_Puerto.Location = new System.Drawing.Point(144, 47);
+            this.comboBox_Puerto.Name = "comboBox_Puerto";
+            this.comboBox_Puerto.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Puerto.TabIndex = 2;
+            this.comboBox_Puerto.SelectedIndexChanged += new System.EventHandler(this.comboBox_Puerto_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione velocidad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione puerto:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_B);
+            this.groupBox2.Controls.Add(this.textBox_G);
+            this.groupBox2.Controls.Add(this.textBox_R);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.trackBar_B);
+            this.groupBox2.Controls.Add(this.trackBar_G);
+            this.groupBox2.Controls.Add(this.trackBar_R);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(289, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(581, 403);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "COLORES RGB";
+            // 
+            // textBox_B
+            // 
+            this.textBox_B.Location = new System.Drawing.Point(520, 324);
+            this.textBox_B.Name = "textBox_B";
+            this.textBox_B.Size = new System.Drawing.Size(55, 20);
+            this.textBox_B.TabIndex = 10;
+            // 
+            // textBox_G
+            // 
+            this.textBox_G.Location = new System.Drawing.Point(520, 253);
+            this.textBox_G.Name = "textBox_G";
+            this.textBox_G.Size = new System.Drawing.Size(55, 20);
+            this.textBox_G.TabIndex = 9;
+            // 
+            // textBox_R
+            // 
+            this.textBox_R.Location = new System.Drawing.Point(520, 190);
+            this.textBox_R.Name = "textBox_R";
+            this.textBox_R.Size = new System.Drawing.Size(55, 20);
+            this.textBox_R.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R";
+            // 
+            // trackBar_B
+            // 
+            this.trackBar_B.Location = new System.Drawing.Point(45, 324);
+            this.trackBar_B.Maximum = 255;
+            this.trackBar_B.Name = "trackBar_B";
+            this.trackBar_B.Size = new System.Drawing.Size(468, 45);
+            this.trackBar_B.TabIndex = 3;
+            // 
+            // trackBar_G
+            // 
+            this.trackBar_G.Location = new System.Drawing.Point(45, 253);
+            this.trackBar_G.Maximum = 255;
+            this.trackBar_G.Name = "trackBar_G";
+            this.trackBar_G.Size = new System.Drawing.Size(468, 45);
+            this.trackBar_G.TabIndex = 2;
+            // 
+            // trackBar_R
+            // 
+            this.trackBar_R.Location = new System.Drawing.Point(45, 191);
+            this.trackBar_R.Maximum = 255;
+            this.trackBar_R.Name = "trackBar_R";
+            this.trackBar_R.Size = new System.Drawing.Size(468, 45);
+            this.trackBar_R.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 147);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PuertoSerie
+            // 
+            this.PuertoSerie.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.PuertoSerie_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(12, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(12, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(30, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "LED";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "90",
+            "180"});
+            this.comboBox1.Location = new System.Drawing.Point(137, 289);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Teal;
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(170, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Servomotor";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(156, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cambiar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(882, 431);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_G)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_R)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonConectar;
+        private System.Windows.Forms.ComboBox comboBox_velocidad;
+        private System.Windows.Forms.ComboBox comboBox_Puerto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_B;
+        private System.Windows.Forms.TextBox textBox_G;
+        private System.Windows.Forms.TextBox textBox_R;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar_B;
+        private System.Windows.Forms.TrackBar trackBar_G;
+        private System.Windows.Forms.TrackBar trackBar_R;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort PuertoSerie;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Actividades/07_Menu basico de PC/Codigo/ARDUINO_RGB/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ARDUINO_RGB
+{
+    public partial class Form1 : Form
+    {
+        string Nombre, velocidad, dato, angulo;
+        int R, G, B;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PuertoSerie.Close();
+            PuertoSerie.Dispose();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            CheckForIllegalCrossThreadCalls = false;
+
+            string[] Puertos = SerialPort.GetPortNames();
+            foreach(string puerto in Puertos)
+            {
+                comboBox_Puerto.Items.Add(puerto);
+            }
+
+            comboBox_velocidad.Items.Insert(0, 9600);
+            comboBox_velocidad.Items.Insert(1, 14400);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PuertoSerie.Write("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PuertoSerie.Write("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            angulo = comboBox1.Text;
+            if (angulo == "0")
+            {
+                PuertoSerie.Write("0");
+            }
+            if (angulo == "90")
+            {
+                PuertoSerie.Write("3");
+            }
+            if (angulo == "180")
+            {
+                PuertoSerie.Write("4");
+            }
+        }
+
+        private void comboBox_Puerto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Nombre = comboBox_Puerto.Text;
+            PuertoSerie.PortName = Nombre;
+        }
+
+        private void comboBox_velocidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            velocidad = comboBox_velocidad.Text;
+            PuertoSerie.BaudRate = Convert.ToInt32(velocidad);
+
+        }
+
+        private void buttonConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckForIllegalCrossThreadCalls = false;
+
+                PuertoSerie.Open();
+                Console.Beep();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Verifique la conexión", "Error");
+            }
+        }
+
+        private void PuertoSerie_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                dato = PuertoSerie.ReadLine();
+
+                if(dato.Contains("R"))
+                {
+                    dato = dato.Remove(0, 1);
+
+                    R = Convert.ToInt32(dato);
+                    trackBar_R.Value = R;
+                    textBox_R.Text = dato;
+                }
+                if (dato.Contains("G"))
+                {
+                    dato = dato.Remove(0, 1);
+
+                    G = Convert.ToInt32(dato);
+                    trackBar_G.Value = G;
+                    textBox_G.Text = dato;
+                }
+                if (dato.Contains("B"))
+                {
+                    dato = dato.Remove(0, 1);
+
+                    B = Convert.ToInt32(dato);
+                    trackBar_B.Value = B;
+                    textBox_B.Text = dato;
+                }
+
+                pictureBox1.BackColor = Color.FromArgb(R, G, B);
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+       
+
+       
+    }
+}
